4239f6743056eaa935a7f182c24cc4759deb71e2
Ale <rvalex36@gmail.com>
2025-10-22 20:29:18 -0400

    feature: added a grid background todo: fix the fading in on the top part

---
+++ src/components/scroll-behavior/grid-fade.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export function GridFade() {
+  return (
+    <div className="fixed inset-0 -z-10 pointer-events-none bg-gradient-to-b from-transparent via-black to-transparent h-1/2 top-1/2" />
+  )
+}
